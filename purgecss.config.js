12b89e6dc6c42062eaaf022d251a62019a595499
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-06-11 21:39:03 +0100

    setup purgecss with config file

---
+++ purgecss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['public/index.html'],
+  css: ['public/styles.css'],
+  extractors: [
+    {
+      extractor: {
+        extract: content =>
+          content.match(/[A-z0-9-:\/]+/g)
+      },
+      extensions: ['html']
+    }
+  ], 
+}
